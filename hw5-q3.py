b3afbed6db50cfca35f1a509627f1bc80a32918a
wedunsto <wedunsto@aggies.ncat.edu>
2020-03-18 20:36:58 -0400

    Finished homework 5

--- hw5-q3.py
+++ hw5-q3.py
@@ -2,6 +2,18 @@
 #Find the longest word in each line
 #Store the longests words in a seperate file
 
-userPath = input("enter a path to a text file")
-with open(userPath, 'r') as file_reader:
-	fileLines = file_reader.readline()
+userPath = input("enter a path to a text file\n") #Store datapath
+maxWordLengths = [] #Create a list that will hold all the longest words in each line
+with open(userPath, 'r') as file_reader: #Opens the text file
+		fileLines = file_reader.read().split('\n') #Stores each line in its own list element
+
+for parts in fileLines: #Iterates over the lines
+	if len(parts) > 0: #Omits the spaces
+		words = parts.split() #Splits each line word by word
+		maxWordLengths.append(max(words, key = lambda word : len(word))) #Stores the largest word per line in a list
+
+staticPath = "./result.txt"
+
+with open(staticPath, 'w') as file_writer: #Opens the destination file
+	for largestWords in maxWordLengths: #Iterate through the list
+		file_writer.write(largestWords+" ") #Stores the largest words in a new file
